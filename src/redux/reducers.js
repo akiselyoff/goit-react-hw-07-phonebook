3d0b037603a56dd68164741e97ba39ac183bc246
kesel <a.kiselyoff@gmail.com>
2022-07-19 01:28:01 +0300

    clear reducers and change store
    delete items slice
    split store reducer to API and filter

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,19 +1,4 @@
-import { createSlice, combineReducers } from '@reduxjs/toolkit';
-
-export const items = createSlice({
-  name: 'items',
-  initialState: [],
-  reducers: {
-    addContact(state, { payload }) {
-      // state.push(payload);//можна мутировать стейт, под капотом код делается иммутабельным
-      return [...state, payload];
-    },
-
-    deleteContact(state, { payload }) {
-      return state.filter(({ id }) => id !== payload);
-    },
-  },
-});
+import { createSlice } from '@reduxjs/toolkit';
 
 export const filter = createSlice({
   name: 'filter',
@@ -25,36 +10,5 @@ export const filter = createSlice({
   },
 });
 
-export const { addContact, deleteContact } = items.actions;
 export const { changeFilter } = filter.actions;
-
-export default combineReducers({
-  items: items.reducer,
-  filter: filter.reducer,
-});
-
-// const items = createReducer([], {//alternative code for use with actions
-//   [addContact]: (state, { payload }) => [...state, payload],
-//   [deleteContact]: (state, { payload }) =>
-//     state.filter(({ id }) => id !== payload),
-// });
-
-// const filter = createReducer('', {
-//   [changeFilter]: (state, { payload }) => payload,
-// });
-
-// export default combineReducers({ items, filter });
-
-// export const items = createSlice({
-//   name: 'items',
-//   initialState: [],
-//   reduces: {
-//     addContact(state, { payload }) {
-//       state = state.push(payload);
-//     },
-//     deleteContact(state, { payload }) {
-//       // state.filter(({ id }) => id !== payload);
-//       state = state.filter(({ id }) => id !== payload);
-//     },
-//   },
-// });
+export default filter.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { contactsApi } from '../API/contactsApi';
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
+import filter from './reducers';
 
 const store = configureStore({
   reducer: {
-    contacts: rootReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
+    filter,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
